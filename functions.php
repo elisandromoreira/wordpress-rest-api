defcc9b45b4db60a33edb65b37e61256e8ddd37d
Elisandro Moreira <elisandro.moreira@univille.br>
2020-10-24 21:22:10 -0300

    Add Create User

--- functions.php
+++ functions.php
@@ -1,6 +1,8 @@
 <?php 
-
 remove_action('rest_api_init', 'create_initial_rest_routes', 99);
+
+$dirbase = get_template_directory();
+require_once $dirbase . '/endpoints/user_post.php';
 
 function change_api() {
     return 'json';
