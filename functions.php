df23eed12be249920fde69c9b3d1685356779836
Elisandro Moreira <elisandro.moreira@univille.br>
2020-10-25 00:07:05 -0300

    Add Create Photo Post

--- functions.php
+++ functions.php
@@ -1,9 +1,15 @@
 <?php 
-remove_action('rest_api_init', 'create_initial_rest_routes', 99);
+//remove_action('rest_api_init', 'create_initial_rest_routes', 99);
 
 $dirbase = get_template_directory();
 require_once $dirbase . '/endpoints/user_post.php';
 require_once $dirbase . '/endpoints/user_get.php';
+
+require_once $dirbase . '/endpoints/photo_post.php';
+
+update_option( 'large_size_w', 1000);
+update_option( 'large_size_h', 1000);
+update_option( 'large_crop', 1);
 
 function change_api() {
     return 'json';
