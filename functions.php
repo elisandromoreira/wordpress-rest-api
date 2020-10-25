153b48e7c191254b7c75a2ea1280453a894bb418
Elisandro Moreira <elisandro.moreira@univille.br>
2020-10-25 01:18:59 -0300

    Add list comments by post

--- functions.php
+++ functions.php
@@ -9,6 +9,7 @@ require_once $dirbase . '/endpoints/photo_post.php';
 require_once $dirbase . '/endpoints/photo_delete.php';
 
 require_once $dirbase . '/endpoints/comment_post.php';
+require_once $dirbase . '/endpoints/comment_get.php';
 
 update_option( 'large_size_w', 1000);
 update_option( 'large_size_h', 1000);
