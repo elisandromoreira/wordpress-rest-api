9b3cb06c3155fd61126452d87c184924e3435938
Elisandro Moreira <elisandro.moreira@univille.br>
2020-10-24 22:52:26 -0300

    Add Get logged user data

---
+++ endpoints/user_get.php
@@ -0,0 +1,32 @@
+<?php 
+
+function api_user_get($request) {
+    $user = wp_get_current_user();
+    $user_id = $user->ID;
+
+    if($user_id === 0) {
+        $response = new WP_Error('error', 'Usuário não autorizado', ['status' => 401]); 
+        return rest_ensure_response($response);
+    }
+
+    $response = [
+        'id' => $user_id,
+        'username' => $user->user_login,
+        'name' => $user->display_name,
+        'email' => $user->user_email,
+    ];
+
+    return rest_ensure_response($response);
+}
+
+
+function register_api_user_get() {
+    register_rest_route('api', '/user', [
+        'methods' => WP_REST_Server::READABLE,
+        'callback' => 'api_user_get',
+    ]);
+}
+
+add_action('rest_api_init', 'register_api_user_get');
+
+?>
--- functions.php
+++ functions.php
@@ -3,6 +3,7 @@ remove_action('rest_api_init', 'create_initial_rest_routes', 99);
 
 $dirbase = get_template_directory();
 require_once $dirbase . '/endpoints/user_post.php';
+require_once $dirbase . '/endpoints/user_get.php';
 
 function change_api() {
     return 'json';
