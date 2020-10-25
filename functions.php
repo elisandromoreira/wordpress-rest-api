ee15d3d7684b2f5c2875971e3fc16bb91a261c50
Elisandro Moreira <elisandro.moreira@univille.br>
2020-10-25 18:31:01 -0300

    Add GET Photo Stats

--- functions.php
+++ functions.php
@@ -20,6 +20,8 @@ require_once $dirbase . '/endpoints/comment_get.php';
 
 require_once $dirbase . '/endpoints/password.php';
 
+require_once $dirbase . '/endpoints/stats_get.php';
+
 update_option( 'large_size_w', 1000);
 update_option( 'large_size_h', 1000);
 update_option( 'large_crop', 1);
